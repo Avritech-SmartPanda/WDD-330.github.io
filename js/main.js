f31fe03f724421c7a163682ef792ea48d581904a
Daphne Avril <daphneavril@outlook.com>
2021-05-14 00:10:59 +0200

    week 03 reading and notes

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
   const links = [
         { label: "Week 01", url: "week01/index.html" },
         { label: "Week 02", url: "week02/index.html" },
-        // { label: "Week 03", url: "week03/index.html" },
+        { label: "Week 03", url: "week03/index.html" },
         // { label: "Week 04", url: "week04/index.html" },
         // { label: "Week 05", url: "week05/index.html" },
         // { label: "Week 06", url: "week06/index.html" },
