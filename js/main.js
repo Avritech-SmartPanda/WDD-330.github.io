df5e0a7f16ef465af40235313e5435a192ddd046
Daphne Avril <daphneavril@outlook.com>
2021-05-16 00:21:38 +0200

    week 04 notes

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
         { label: "Week 01", url: "week01/index.html" },
         { label: "Week 02", url: "week02/index.html" },
         { label: "Week 03", url: "week03/index.html" },
-        // { label: "Week 04", url: "week04/index.html" },
+        { label: "Week 04", url: "week04/index.html" },
         // { label: "Week 05", url: "week05/index.html" },
         // { label: "Week 06", url: "week06/index.html" },
         // { label: "Week 07", url: "week07/index.html" },
