e87c10b72b6ece630df92520e4273aad15bab2fc
DaphneAvril2604 <78037754+DaphneAvril2604@users.noreply.github.com>
2021-09-18 20:46:24 +0200

    Week 01 - fall - 2021

--- js/main.js
+++ js/main.js
@@ -1,18 +1,6 @@
 
 const links = [
-    { label: "Week 01", url: "week01/index.html" },
-    { label: "Week 02", url: "week02/index.html" },
-    { label: "Week 03", url: "week03/index.html" },
-    { label: "Week 04", url: "week04/index.html" },
-    { label: "Week 05", url: "week05/index.html" },
-    { label: "Week 06", url: "week06/index.html" },
-    { label: "Week 07", url: "week07/index.html" },
-    { label: "Week 08", url: "week08/index.html" },
-    { label: "Week 09", url: "week09/index.html" },
-    // { label: "Week 10", url: "week10/index.html" },
-    // { label: "Week 11", url: "week11/index.html" },
-    // { label: "Week 12", url: "week12/index.html" },
-    // { label: "Week 13", url: "week13/index.html" }
+    { label: "Week 01", url: "week01/index.html" }
 ]
 
 function embedLinks() {
